c5e5e32a651c1a1cf7e03788bbadb84134967cc2
Miguel Ángel Bueno Rivera <magalenyo7@gmail.com>
2020-06-22 20:25:24 +0200

    Init Commit

---
+++ zork/zork/Entity.h
@@ -0,0 +1,38 @@
+/**
+* Entity.h
+*
+*
+* Created: 20/06/2020
+*
+* Author: Miguel Ángel Bueno Rivera
+*
+*/
+
+#ifndef Entity_h
+#define Entity_h
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Entity
+{
+// VARIABLES
+public:
+protected:
+private:
+	vector<Entity> _elements;
+	string _name;
+	string _description;
+
+
+//FUNCTIONS
+public:
+protected:
+private:
+
+};
+
+#endif
+
