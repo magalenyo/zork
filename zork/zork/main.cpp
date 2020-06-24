697ac1ab9c75b5de392fc931dbbd455a5511cb37
Miguel Ángel Bueno Rivera <magalenyo7@gmail.com>
2020-06-24 08:16:39 +0200

    Main initial structure
    Added tokenize function and the main has a small body

--- zork/zork/main.cpp
+++ zork/zork/main.cpp
@@ -4,19 +4,41 @@ using namespace std;
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream> 
+#include <World.h>
 #include <Commands.h>
+
+// pre: param is a string separated by spaces
+// post: returns a vector of string; this vector is the param tokenized
+vector<string> tokenize(string param) {
+	vector<string> result;
+
+	istringstream iss( param );
+	string str;
+	while (iss >> str) {
+		result.push_back(str);
+	}
+
+	return result;
+}
 
 int main()
 {
 	cout << "Welcome to ZORK." << endl;
-	string input;
-	//getline(cin, input);
-	while (cin >> input) {
-		cout << input << endl;
-	}
+	World world;
 	
-	cout << Commands::NORTH.default << " " << Commands::NORTH.shortcut << endl;
 
+	string input;
+	getline(cin, input);
+	vector<string> arguments = tokenize(input);
+
+	while (!world.isEndCommand(arguments)) {
+		
+		getline(cin, input);
+		arguments = tokenize(input);
+	}
+	cout << "Bye." << endl;
+	
 	return 0;
-	//while(input.)
 }
